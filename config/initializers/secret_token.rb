89ae8d3a4bbca41e23063dcba9a39c7173670290
Clemens Park <clemens.park@gmail.com>
2010-11-19 16:33:56 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ConnectFour::Application.config.secret_token = 'b8b444048bef2b7253aa217641fc41c708d597e91621a5a840b8bb236e349451592b61bcd2bd139324620166dddce2ac92b1748dcf8ae2c570197b32f0c57a73'
